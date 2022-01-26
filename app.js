c5c7b327c10c74bc47399a92e145ff414ac6c449
user <user@MacBook-Pro-de-user.local>
2022-01-26 15:57:49 -0300

    login y register

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
-const express = require ('express') 
-const path = require ('path');
+const express = require('express');
+const app = express();
 
-const app = express (); 
-
-app.use(express.static('./public'));
-
-
-app.get ('/', (req,res) => {
-    const archivo = path.join (__dirname, './views/home.html')
-    res.sendFile(archivo); 
-})
+const path = require('path');
+const pathPublic = path.join(__dirname, './public');
+app.use(express.static(pathPublic));
 
 
-app.listen(3001)
+app.listen(process.env.PORT || 3001, function () { console.log('Servidor corriendo en: http://localhost:3001/')});
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, './views/home.html'))
+});
+
+app.get('/home', (req,res) => {
+    res.sendFile(path.join(__dirname, './views/home.html'))
+});
+
+app.get('/register', (req,res) => {
+    res.sendFile(path.join(__dirname, './views/register.html'))
+});
+
+app.get('/login', (req,res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'))
+});
+
+
+
